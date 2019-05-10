e4207f36201751f0cedb69216366c8ba96901f40
Dnhy <he.yu2@ztesoft.com>
2019-05-10 13:16:49 +0800

    two

--- microservicecloud/microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
+++ microservicecloud/microservicecloud-api/src/main/java/com/atguigu/springcloud/entities/Dept.java
@@ -19,10 +19,7 @@ public class Dept implements Serializable{
 	//部门名称
 	private String deptname;
 	//采用那个数据库
-	private String db_source;
-	//名字
-	private String  name;
-	
+	private String db_source;	
 	public static void main(String[] args) {
 		Dept dept=new Dept();
 		dept.setDeptname("heyu").setDeptno(88L).setDb_source("day01");
