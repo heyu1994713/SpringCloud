d15ef2b62249db7e02416ce1a46e799b21e2dfdb
Dnhy <he.yu2@ztesoft.com>
2019-05-11 23:29:19 +0800

    配置中心服务端

---
+++ microservicecloud/microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/Config_ClientApp.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Config_ClientApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Config_ClientApp.class,args);
+	}
+
+}
